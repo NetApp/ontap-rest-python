6a39ed41e800a1bef96fe2697f91d20439a3dd03
Robert Blackhart <robert@robertblackhart.com>
2020-05-21 11:03:12 -0400

    added a software upgrade script example

--- examples/python_client_library/utils.py
+++ examples/python_client_library/utils.py
@@ -23,12 +23,13 @@ STEP_INDEX = 1
 
 class Argument:  # pylint: disable=too-few-public-methods
     """A structure to hold details of an argument"""
-    def __init__(self, short_arg: str, long_arg: str, help_string: str, default=None, required=False):
+    def __init__(self, short_arg: str, long_arg: str, help_string: str, default=None, required=False, arg_type=None):
         self.short_arg = short_arg
         self.long_arg = long_arg
         self.help_string = help_string
         self.default = default
         self.required = required
+        self.arg_type = arg_type if arg_type else str
 
 
 def parse_args(program_description: str, arguments: List[Argument]) -> argparse.Namespace:
@@ -38,7 +39,7 @@ def parse_args(program_description: str, arguments: List[Argument]) -> argparse.
     for argument in arguments:
         parser.add_argument(
             argument.short_arg, argument.long_arg, required=argument.required,
-            help=argument.help_string, default=argument.default,
+            help=argument.help_string, default=argument.default, type=argument.arg_type,
         )
     parser.add_argument("-u", "--api_user", default="admin", help="API Username")
     parser.add_argument("-p", "--api_pass", help="API Password")
@@ -134,3 +135,44 @@ def run_cmd(command: Union[List[str], str]) -> None:
         result.check_returncode()
     else:
         logging.info("<<< %s", result.stdout.decode("utf-8"))
+
+
+class LiveMultilineOutput:
+    """This is a class for managing multiline text output on the screen.
+
+    While inside this class' context, the application can change the output's
+    buffer by calling change() and the buffer will be redrawn, clearing the
+    previous output and printing all of the new output. Once the context is
+    exited, the cursor is moved to the line after the last line in the output
+    so that normal printing can continue.
+    """
+
+    def __init__(self, initial_data=None):
+        self.buffer = []
+        if initial_data:
+            self.buffer = initial_data
+        self._lines_drawn = 0
+        self.draw()
+
+    def change(self, new_list):
+        """Update the text buffer with a new list of strings"""
+
+        self.buffer = new_list
+        self.draw()
+
+    def draw(self):
+        """Erase our current line and write a new one, then move down"""
+
+        for line in self.buffer:
+            print("\033[K%s" % line)
+        self._lines_drawn = len(self.buffer)
+        # now that we're done, move all the way back to the top left
+        if self._lines_drawn > 0:
+            print("\033[%sA\r" % self._lines_drawn, end="")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        # move down to the bottom
+        print("\033[%sB" % (self._lines_drawn - 1))
