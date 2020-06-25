977874d9958569ec88acd9ef0a8c1983746f6821
Mahalakshmi G <61284863+mahaganesan11@users.noreply.github.com>
2020-06-25 19:48:19 +0530

    Update list_volume.py

--- examples/python_client_library/list_volume.py
+++ examples/python_client_library/list_volume.py
@@ -6,7 +6,7 @@ This script was developed by NetApp to help demonstrate NetApp
 technologies.  This script is not officially supported as a
 standard NetApp product.
 
-Purpose: Script to list aggregates using ONTAP REST API Python Client Library.
+Purpose: Script to list volumes using ONTAP REST API Python Client Library.
 
 usage: python3 list_volume.py [-h] -c CLUSTER -vs SVM_NAME [-u API_USER]
                           [-p API_PASS]
@@ -21,20 +21,16 @@ https://opensource.org/licenses/BSD-3-Clause
 
 from netapp_ontap import NetAppRestError
 from netapp_ontap.resources import Volume
-
 from utils import Argument, parse_args, setup_logging, setup_connection
-
 
 def list_volume_pycl(svm_name: str) -> None:
     """List Volumes in a SVM """
-
     print("\n List of Volumes:- \n")
     try:
         print(*(vol.name for vol in Volume.get_collection(**
                                                           {"svm.name": svm_name})), sep="\n")
     except NetAppRestError as err:
         print("Error: Volume list  was not created: %s" % err)
-
 
 def main() -> None:
     """Main function"""
@@ -49,7 +45,6 @@ def main() -> None:
     setup_connection(args.cluster, args.api_user, args.api_pass)
 
     list_volume_pycl(args.svm_name)
-
 
 if __name__ == "__main__":
     main()
