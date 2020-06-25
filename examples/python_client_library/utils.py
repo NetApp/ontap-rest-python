0362560e24d27448861b1a6b23c7250b02a138eb
Mahalakshmi G <61284863+mahaganesan11@users.noreply.github.com>
2020-06-25 15:23:54 +0530

    Add files via upload

--- examples/python_client_library/utils.py
+++ examples/python_client_library/utils.py
@@ -13,9 +13,10 @@ from getpass import getpass
 import logging
 import subprocess
 from typing import List, Union
-
+from netapp_ontap.resources import Svm, Volume, Aggregate, Snapshot, SnapmirrorRelationship, Qtree, QuotaRule, Igroup
+from netapp_ontap.resources import IgroupInitiator, Disk, Node, IpInterface, Lun
 from netapp_ontap import config, HostConnection
-
+from netapp_ontap import NetAppRestError
 
 SUBSTEP_INDEX = 1
 STEP_INDEX = 1
@@ -23,25 +24,37 @@ STEP_INDEX = 1
 
 class Argument:  # pylint: disable=too-few-public-methods
     """A structure to hold details of an argument"""
-    def __init__(self, short_arg: str, long_arg: str, help_string: str, default=None, required=False, arg_type=None):
+
+    def __init__(
+            self,
+            short_arg: str,
+            long_arg: str,
+            help_string: str,
+            default=None,
+            required=False):
         self.short_arg = short_arg
         self.long_arg = long_arg
         self.help_string = help_string
         self.default = default
         self.required = required
-        self.arg_type = arg_type if arg_type else str
 
 
-def parse_args(program_description: str, arguments: List[Argument]) -> argparse.Namespace:
+def parse_args(
+        program_description: str,
+        arguments: List[Argument]) -> argparse.Namespace:
     """Parse the command line arguments from the user"""
 
     parser = argparse.ArgumentParser(description=program_description)
     for argument in arguments:
         parser.add_argument(
             argument.short_arg, argument.long_arg, required=argument.required,
-            help=argument.help_string, default=argument.default, type=argument.arg_type,
+            help=argument.help_string, default=argument.default,
         )
-    parser.add_argument("-u", "--api_user", default="admin", help="API Username")
+    parser.add_argument(
+        "-u",
+        "--api_user",
+        default="admin",
+        help="API Username")
     parser.add_argument("-p", "--api_pass", help="API Password")
     parsed_args = parser.parse_args()
 
@@ -127,7 +140,10 @@ def run_cmd(command: Union[List[str], str]) -> None:
         run_in_shell = True
         logging.info(">>> %s", command)
     result = subprocess.run(
-        command, shell=run_in_shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        command,
+        shell=run_in_shell,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
         check=True,
     )
     if result.returncode != 0:
@@ -137,42 +153,263 @@ def run_cmd(command: Union[List[str], str]) -> None:
         logging.info("<<< %s", result.stdout.decode("utf-8"))
 
 
-class LiveMultilineOutput:
-    """This is a class for managing multiline text output on the screen.
+def show_aggregate() -> None:
+    """Lists the Aggregate"""
+    print("\n List of Aggregates:- \n")
+    try:
+        for aggregatelist in Aggregate.get_collection():
+            print(aggregatelist.name)
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
 
-    While inside this class' context, the application can change the output's
-    buffer by calling change() and the buffer will be redrawn, clearing the
-    previous output and printing all of the new output. Once the context is
-    exited, the cursor is moved to the line after the last line in the output
-    so that normal printing can continue.
-    """
 
-    def __init__(self, initial_data=None):
-        self.buffer = []
-        if initial_data:
-            self.buffer = initial_data
-        self._lines_drawn = 0
-        self.draw()
+def show_svm() -> None:
+    """Lists SVM"""
+    print()
+    print("Getting SVM Details")
+    print("===================")
+    try:
+        for svm in Svm.get_collection(fields="uuid"):
+            print("SVM name:-%s ; SVM uuid:-%s " % (svm.name, svm.uuid))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
 
-    def change(self, new_list):
-        """Update the text buffer with a new list of strings"""
 
-        self.buffer = new_list
-        self.draw()
+def show_volume(svm_name) -> None:
+    """Lists Volumes"""
+    print()
+    print("Getting Volume Details")
+    print("======================")
+    try:
+        for volume in Volume.get_collection(
+                **{"svm.name": svm_name}, fields="uuid"):
+            print(
+                "Volume Name = %s;  Volume UUID = %s" %
+                (volume.name, volume.uuid))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
 
-    def draw(self):
-        """Erase our current line and write a new one, then move down"""
 
-        for line in self.buffer:
-            print("\033[K%s" % line)
-        self._lines_drawn = len(self.buffer)
-        # now that we're done, move all the way back to the top left
-        if self._lines_drawn > 0:
-            print("\033[%sA\r" % self._lines_drawn, end="")
+def get_key_svm(svm_name):
+    """Lists SVM"""
+    print()
+    print("Getting SVM Details")
+    print("===================")
+    try:
+        for svm in Svm.get_collection(fields="uuid"):
+            if svm.name == svm_name:
+                print(svm.uuid)
+                return svm.uuid
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
 
-    def __enter__(self):
-        return self
 
-    def __exit__(self, exception_type, exception_value, traceback):
-        # move down to the bottom
-        print("\033[%sB" % (self._lines_drawn - 1))
+def get_key_volume(svm_name, volume_name) -> None:
+    """Lists Volumes"""
+    print()
+    print("Getting Volume Details")
+    print("======================")
+    try:
+        for volume in Volume.get_collection(
+                **{"svm.name": svm_name}, fields="uuid"):
+            if volume.name == volume_name:
+                print(volume.uuid)
+                return volume.uuid
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_node() -> None:
+    """List nodes"""
+    print(" Getting Node Details")
+    print("=====================")
+
+    try:
+        for node in Node.get_collection(fields="uuid"):
+            print("Node name:-%s ; Node uuid:-%s " % (node.name, node.uuid))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_snapshot(svm_name, volume_name) -> None:
+    """List Snapshots in a volume"""
+    print()
+    vol_uuid = get_key_volume(svm_name, volume_name)
+    print("The List of Snapshots:-")
+    print("=======================")
+    try:
+        for snapshot in Snapshot.get_collection(vol_uuid):
+            print(snapshot.name)
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_snapmirror() -> None:
+    """List Snapmirror"""
+    print("List of SnapMirror Relationships:")
+    print("=================================")
+
+    try:
+        for snapmirrorrelationship in SnapmirrorRelationship.get_collection(
+                fields="uuid"):
+            print(snapmirrorrelationship.uuid)
+            snapmirror1 = SnapmirrorRelationship.find(
+                uuid=snapmirrorrelationship.uuid)
+            print(snapmirror1.source.path)
+            print(snapmirror1.destination.path)
+            print(snapmirror1.state)
+            print("-----------------------------")
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_qtree(svm_name, volume_name):
+    """List Qtrees in a Volume"""
+    vol_uuid = get_key_volume(svm_name, volume_name)
+    print("The List of Qtrees:-")
+    print("====================")
+    try:
+        for qtree in Qtree.get_collection(**{"volume.uuid": vol_uuid}):
+            print("Name:- %s  ID:- %s" % (qtree.name, qtree.id))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_quotarule() -> None:
+    """Lists Quota Rule"""
+    print()
+    print("Getting Quota Rule Details")
+    print("==========================")
+    try:
+        for quotarule in QuotaRule.get_collection():
+            print(
+                "Quota-Rule UUID = %s;  Volume Name = %s" %
+                (quotarule.uuid, quotarule.volume.name))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def get_key_quotarule_qtree(svm_name, volume_name, qtree_name) -> None:
+    """Lists Quota Rule"""
+    print()
+    print("Getting Quota Rule Details")
+    print("==========================")
+    try:
+        for quotarule in QuotaRule.get_collection(
+                **{'svm.name': svm_name, 'volume.name': volume_name, 'qtree.name': qtree_name}):
+            print(
+                "Quota-Rule UUID = %s;  Volume Name = %s" %
+                (quotarule.uuid, quotarule.qtree.name))
+            return quotarule.uuid
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def get_key_quotarule_volume(svm_name, volume_name) -> None:
+    """Lists Quota Rule"""
+    print()
+    print("Getting Quota Rule Details")
+    print("==========================")
+    try:
+        for quotarule in QuotaRule.get_collection(
+                **{'svm.name': svm_name, 'volume.name': volume_name}):
+            print(
+                "Quota-Rule UUID = %s;  Volume Name = %s" %
+                (quotarule.uuid, quotarule.volume.name))
+            return quotarule.uuid
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_igroup(svm_name) -> None:
+    print()
+    print("Getting Initiator Group Details")
+    print("===============================")
+    try:
+        for igroup in Igroup.get_collection(
+                **{"svm.name": svm_name}, fields="uuid"):
+            print(
+                "Igroup Name = %s;  Igroup UUID = %s" %
+                (igroup.name, igroup.uuid))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_initiator(svm_name, igroup_name) -> None:
+    igroup_uuid = get_key_igroup(svm_name, igroup_name)
+    try:
+        for ini in IgroupInitiator.get_collection(
+                igroup_uuid):
+            print(
+                "Initiator Name = %s " %
+                (ini.name))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def get_key_igroup(svm_name, igroup_name):
+    print()
+    print("Getting Initiator Group Details")
+    print("===============================")
+    try:
+        for igroup in Igroup.get_collection(
+                **{"svm.name": svm_name}, name=igroup_name):
+            print(
+                "Igroup Name = %s;  Igroup UUID = %s" %
+                (igroup.name, igroup.uuid))
+            return igroup.uuid
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_disk():
+    print()
+    print("Getting Disk Details")
+    print("===============================")
+    try:
+        for disk in Disk.get_collection():
+            print(
+                "Disk  Name = %s  " %
+                (disk.name))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_interface():
+    """ List Interface"""
+    print("\n List of Interface:- \n")
+    try:
+        for interface in IpInterface.get_collection():
+            print(
+                "Interface Name:- %s; Inteface UUID:- %s " %
+                (interface.name, interface.uuid))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
+
+
+def show_lun():
+    """ List LUN"""
+    print("\n List of LUN:- \n")
+    try:
+        for lun in Lun.get_collection():
+            print("Lun Name:- %s; Lun UUID:- %s " % (lun.name, lun.uuid))
+    except NetAppRestError as error:
+        print("Error:- " % error.http_err_response.http_response.text)
+        print("Exception caught :" + str(error))
